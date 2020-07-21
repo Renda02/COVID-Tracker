b9c285954a4ab9e651f4f474b8300f225668b569
Renda02 <64105005+Renda02@users.noreply.github.com>
2020-07-21 03:29:08 +0200

    Cleanup the code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_header">
+        Hello World
+        <FormControl>
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">One</MenuItem>
+            <MenuItem value="worldwide">Two</MenuItem>
+          </Select>
+        </FormControl>
+        {/*Header */}
+        {/*Title +Select input dropdown*/}
+        {/*InfoBoxs*/}
+        {/*InfoBoxs*/}
+        {/*InfoBoxs*/}
+        {/*Table*/}
+        {/*Graph*/}
+        {/*Map*/}
+      </div>
     </div>
   );
 }
